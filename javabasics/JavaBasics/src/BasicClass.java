aebf7bbadacdf605cdf9d058806fd578b12b1d8d
Durga Bhavani Kuppagiri <bhavani.kuppagiri@gmail.com>
2020-11-28 09:44:21 -0500

    Introduced a basic class
    This class will have a two variables and a method to print out the variables.

---
+++ javabasics/JavaBasics/src/BasicClass.java
@@ -0,0 +1,16 @@
+
+public class BasicClass {
+
+	//class is basically a blur print /template that describes the state/behavior of the object of its type support
+	
+	/*
+	 * This is a basic class with state age and name
+	 * This class have a method called describeThePerson, which gives the information of the user */
+	private int age =10;
+	private String Name = "Bhavani";
+	
+	
+	public void describeThePerson() {
+		System.out.println("Hi my name is "+Name+" and iam "+age);
+	}
+}
