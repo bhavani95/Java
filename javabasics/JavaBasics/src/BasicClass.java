1540fe9f11f987ccf945e16d90e9362b1b5feb2a
Durga Bhavani Kuppagiri <bhavani.kuppagiri@gmail.com>
2020-11-28 09:56:42 -0500

    Added getters and setters BasicClass.java
    Added getters and setters for the private variables we have in basicClass and accessed them in helloworld.java

--- javabasics/JavaBasics/src/BasicClass.java
+++ javabasics/JavaBasics/src/BasicClass.java
@@ -6,11 +6,29 @@ public class BasicClass {
 	/*
 	 * This is a basic class with state age and name
 	 * This class have a method called describeThePerson, which gives the information of the user */
-	private int age =10;
-	private String Name = "Bhavani";
+	private int age ;
+	private String Name;
 	
+	//how to create getters and setters??
+	//It is very easy now, just right click on here select source and select getters and setters
 	
 	public void describeThePerson() {
 		System.out.println("Hi my name is "+Name+" and iam "+age);
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public String getName() {
+		return Name;
+	}
+
+	public void setName(String name) {
+		Name = name;
 	}
 }
--- javabasics/JavaBasics/src/HelloWorld.java
+++ javabasics/JavaBasics/src/HelloWorld.java
@@ -27,8 +27,19 @@ public class HelloWorld {
       
       BasicClass bhavani = new BasicClass(); 
       
+    
+      
+      //how to set dynamic values to the private variables existed in basicClass
+      //by creating the getters and setters we can get and set dynamic values 
+      bhavani.setAge(15);
+      bhavani.setName("Bhavani Kuppagiri");
+      
       //now bhavani the object have access to the method describeThePerson() method
       bhavani.describeThePerson();
+      
+      //getters usage
+      System.out.println("bhavani age retrived using getAge is "+bhavani.getAge()+" bhavani name retrived from getName is "+bhavani.getName());
+      
       
       
 	}
