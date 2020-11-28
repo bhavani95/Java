82cad4388be993800839483f2ced173dfa6ec6a8
Durga Bhavani Kuppagiri <bhavani.kuppagiri@gmail.com>
2020-11-28 09:49:19 -0500

    Created a new object for BasicClass
    This commit contains the object creation using keyword new.

--- javabasics/JavaBasics/src/HelloWorld.java
+++ javabasics/JavaBasics/src/HelloWorld.java
@@ -16,6 +16,21 @@ public class HelloWorld {
 		
 		// TODO Auto-generated method stub
       System.out.println("Hi this is Bhavani and iam starting my java practice");
+      //create an object to the BasicClass.java
+      /*what is an object?
+       * An object is an instance of a class
+       * how to create an object??
+       * Object can be created using the key word new
+       * example: BasicClass bhavani = new BasicClass(); 
+       * If we create an objcet we can access all the variables and methods declared as public
+       * */
+      
+      BasicClass bhavani = new BasicClass(); 
+      
+      //now bhavani the object have access to the method describeThePerson() method
+      bhavani.describeThePerson();
+      
+      
 	}
 
 }
